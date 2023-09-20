f2a6707a4f986dddf075c03e1a39129bba245873
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-09-20 09:51:13 -0300

    Adds word generator for creating big text files

---
+++ word_generator/wordgenerator.py
@@ -0,0 +1,12 @@
+from lorem.text import TextLorem
+
+FILE_SIZE = 1500000000
+print("starting file generation")
+lorem = TextLorem()
+
+with open("large_lorem.txt", "w", encoding="utf-8") as file:
+    while file.tell() < FILE_SIZE:
+        text = lorem.paragraph()
+        file.write(text)
+print("file generation complete")
+
