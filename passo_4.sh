9b1b043632cf7a2bed1f1cd87d5d5cb1c41ea377
Renato Britto <renatobritto@protonmail.com>
2023-11-28 15:31:14 -0300

    fix mashing hadoop configurations

--- passo_4.sh
+++ passo_4.sh
@@ -1,18 +1,29 @@
-cd word_generator
-pip install -r requirements.txt
-python3 wordgenerator.py
+echo "-----> gerando palavras"
+cd word_generator ;\
+pip install -r requirements.txt ;\
+python3 wordgenerator.py ;\
 
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -mkdir /input'
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -rm /user/root/output/*'
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -rmdir /user/root/output'
+echo "-----> reiniciando servicos"
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/sbin/stop-dfs.sh'
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/sbin/stop-yarn.sh'
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/sbin/start-dfs.sh'
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/sbin/start-yarn.sh'
 
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -rm /input/input.txt'
+echo "-----> ajustando hdfs para receber resposta (removendo arquivos antigos)"
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -mkdir /input' ;\
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -rm /user/root/output/*' ;\
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -rmdir /user/root/output' ;\
 
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -put ./word_generator/large_lorem.txt /input/input.txt'
+# sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -rm /input/input.txt' ;\
 
-rm word_generator/large_lorem.txt
+echo "-----> copiando input pra hdfs"
+sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/bin/hdfs dfs -put ./word_generator/large_lorem.txt /input/input.txt' ;\
 
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c 'cd WordCount; javac -classpath ${HADOOP_PREFIX}/share/hadoop/common/hadoop-common-2.7.0.jar:${HADOOP_PREFIX}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.0.jar:${HADOOP_PREFIX}/share/hadoop/common/lib/* Wordcount.java; jar cvf wordcount.jar *.class'
+# rm word_generator/large_lorem.txt ;\
 
-sudo docker-compose exec hadoop-slave-1 /bin/bash -c 'cd WordCount && $HADOOP_PREFIX/bin/hadoop jar wordcount.jar Wordcount /input/input.txt output'
+echo "-----> compilando programa de wordcount"
+sudo docker-compose exec hadoop-master /bin/bash -c 'cd WordCount; javac -classpath ${HADOOP_PREFIX}/share/hadoop/common/hadoop-common-2.7.0.jar:${HADOOP_PREFIX}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.0.jar:${HADOOP_PREFIX}/share/hadoop/common/lib/* Wordcount.java; jar cvf wordcount.jar *.class' ;\
+
+echo "-----> rodando word count"
+sudo docker-compose exec hadoop-master /bin/bash -c 'cd WordCount && $HADOOP_PREFIX/bin/hadoop jar wordcount.jar Wordcount /input/input.txt output'
 
--- roda_hadoop_3_nodes.sh
+++ roda_hadoop_3_nodes.sh
@@ -1,28 +1,28 @@
 # referencias:
 # https://www.linode.com/docs/guides/how-to-install-and-set-up-hadoop-cluster/
 
-# incia os containers
+echo "-----> incia os containers"
 sudo docker-compose down -v
 sudo docker-compose up --remove-orphans -d
 
-# set hosts
+echo "-----> set hosts"
 sudo docker-compose exec hadoop-master /bin/bash -c 'echo "172.20.0.2 hadoop-master" >> /etc/hosts'
 sudo docker-compose exec hadoop-master /bin/bash -c 'echo "172.20.0.3 hadoop-slave-1" >> /etc/hosts'
 sudo docker-compose exec hadoop-master /bin/bash -c 'echo "172.20.0.4 hadoop-slave-2" >> /etc/hosts'
 
-# create ssh key
+echo "-----> create ssh key"
 sudo docker-compose exec hadoop-master /bin/bash -c 'ssh-keygen -b 4096 -rsa -N ""'
 
-# copy ssh pub key (other containers share this folder)
+echo "-----> copy ssh pub key (other containers share this folder)"
 sudo docker cp hadoop-master:/root/.ssh/id_rsa.pub master.pub
 sudo docker cp master.pub hadoop-slave-1:/root/.ssh/authorized_keys
 sudo docker cp master.pub hadoop-slave-2:/root/.ssh/authorized_keys
 sudo rm master.pub
 
-# formata o hdfs
+echo "-----> formata o hdfs"
 sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/bin/hdfs namenode -format'
 
-# reinicia dfs
+echo "-----> reinicia dfs"
 sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/sbin/stop-dfs.sh'
 sudo docker-compose exec hadoop-master /bin/bash -c '$HADOOP_PREFIX/sbin/start-dfs.sh'
 
